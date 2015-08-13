9f0db45b84295391b2a56958262003b3e30e2719
sandeepchhapola <schhapola0001@gmail.com>
2015-08-13 18:44:58 +0530

    Injected angular-route in angular app for design section

--- control/design/app.js
+++ control/design/app.js
@@ -1,6 +1,14 @@
 'use strict';
 (function (angular, buildfire) {
   angular
-    .module('youtubePluginDesign', ['ngAnimate', 'ngRoute',  'ui.bootstrap'])
-
+    .module('youtubePluginDesign', ['ngRoute'])
+      .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+              templateUrl: 'templates/home.html',
+              controllerAs: 'DesignHome',
+              controller: 'DesignHomeCtrl'
+            })
+            .otherwise('/');
+      }])
 })(window.angular, window.buildfire);
