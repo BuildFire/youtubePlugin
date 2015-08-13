017c469e71a4192567361000561f800b890837d6
sandeepchhapola <schhapola0001@gmail.com>
2015-08-13 18:38:21 +0530

    Injected angular-route in angular app for content section

--- control/content/app.js
+++ control/content/app.js
@@ -1,6 +1,14 @@
 'use strict';
 
 (function (angular) {
-  angular.module('youtubePluginContent', ['ui.bootstrap', 'ui.tinymce']);
-
+  angular.module('youtubePluginContent', ['ngRoute'])
+      .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+              templateUrl: 'templates/home.html',
+              controllerAs: 'ContentHome',
+              controller: 'ContentHomeCtrl'
+            })
+            .otherwise('/');
+      }])
 })(window.angular);
