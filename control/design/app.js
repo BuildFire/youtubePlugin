79cfd3d20aafae68dd4850dde41e6fc8c1a006c8
sandeepchhapola <schhapola0001@gmail.com>
2015-08-17 13:45:22 +0530

    Showed list layout images

--- control/design/app.js
+++ control/design/app.js
@@ -11,4 +11,18 @@
             })
             .otherwise('/');
       }])
+      .filter('getImageUrl', function () {
+          return function (url, width, height, type) {
+              if (type == 'resize')
+                  return buildfire.imageLib.resizeImage(url, {
+                      width: width,
+                      height: height
+                  });
+              else
+                  return buildfire.imageLib.cropImage(url, {
+                      width: width,
+                      height: height
+                  });
+          }
+      });
 })(window.angular, window.buildfire);
