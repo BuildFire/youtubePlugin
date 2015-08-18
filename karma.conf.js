32d1a2ea43416815ccf122187f1a24b56fb6600f
sandeepchhapola <schhapola0001@gmail.com>
2015-08-18 18:09:30 +0530

    Moved 'js' folder into assets and Excluded 'assets' folder from test coverage

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,7 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'control/**/*.js':['coverage'],
+            'control/**/!(assets)/*.js':['coverage'],
             'widget/**/*.js':['coverage']
         },
 
