9936efaff3a158b5451a06338c84a98606caffa2
sandeepchhapola <schhapola0001@gmail.com>
2015-08-18 17:50:44 +0530

    Modified test cases in content.home..controller.spec.js

--- test/control/content/content.home.controller.spec.js
+++ test/control/content/content.home.controller.spec.js
@@ -43,16 +43,16 @@ describe('Unit : youtubePlugin content.home.controller.js', function () {
     describe('ContentHome.masterData', function () {
         it('it should pass if ContentHome.masterData match the result', function () {
             expect(ContentHome.masterData).toEqual({
-                "content": {
-                    "images": [],
-                    "description": '<p><br data-mce-bogus="1"></p>',
-                    "rssUrl": "",
-                    "type": CONTENT_TYPE.SINGLE_VIDEO
+                content: {
+                    images: [],
+                    description: '<p><br data-mce-bogus="1"></p>',
+                    rssUrl: '',
+                    type: ''
                 },
-                "design": {
-                    "itemListLayout": "",
-                    "itemListBgImage": "",
-                    "itemDetailsBgImage": ""
+                design: {
+                    itemListLayout: '',
+                    itemListBgImage: '',
+                    itemDetailsBgImage: ''
                 }
             });
         });
@@ -65,64 +65,21 @@ describe('Unit : youtubePlugin content.home.controller.js', function () {
     });
 
     describe('ContentHome.data', function () {
-        it('it should pass if ContentHome.data is null', function () {
+        it('it should pass if ContentHome.data is match the result', function () {
             expect(ContentHome.data).toEqual({
-              "content": {
-                "images": [],
-                "description": '<p><br data-mce-bogus="1"></p>',
-                "rssUrl": "",
-                "type": CONTENT_TYPE.SINGLE_VIDEO
-              },
-              "design": {
-                "itemListLayout": "",
-                "itemListBgImage": "",
-                "itemDetailsBgImage": ""
-              }
+                content: {
+                    images: [],
+                    description: '<p><br data-mce-bogus="1"></p>',
+                    rssUrl: '',
+                    type: ''
+                },
+                design: {
+                    itemListLayout: '',
+                    itemListBgImage: '',
+                    itemDetailsBgImage: ''
+                }
             });
         });
     });
-
-    describe('Function : ContentHome.changeContentType', function () {
-        it('ContentHome.changeContentType should exist and be a function', function () {
-            expect(typeof ContentHome.changeContentType).toEqual('function');
-        });
-        it('it should pass if ContentHome.data.content.type is equals to CONTENT_TYPE.CHANNEL_FEED after passing parameter "Channel Feed" to ContentHome.changeContentType', function () {
-            ContentHome.data = {
-                "content": {
-                    "images": [],
-                    "description": '<p><br data-mce-bogus="1"></p>',
-                    "rssUrl": "",
-                    "type": ''
-                },
-                "design": {
-                    "itemListLayout": "",
-                    "itemListBgImage": "",
-                    "itemDetailsBgImage": ""
-                }
-            };
-            ContentHome.changeContentType("Channel Feed");
-            $rootScope.$digest();
-            expect(ContentHome.data.content.type).toEqual(CONTENT_TYPE.CHANNEL_FEED);
-        });
-        it('it should pass if ContentHome.data.content.type is equals to CONTENT_TYPE.SINGLE_VIDEO after passing parameter "Single Video" to ContentHome.changeContentType', function () {
-            ContentHome.data = {
-                "content": {
-                    "images": [],
-                    "description": '<p><br data-mce-bogus="1"></p>',
-                    "rssUrl": "",
-                    "type": ''
-                },
-                "design": {
-                    "itemListLayout": "",
-                    "itemListBgImage": "",
-                    "itemDetailsBgImage": ""
-                }
-            };
-            ContentHome.changeContentType(CONTENT_TYPE.SINGLE_VIDEO);
-            $rootScope.$digest();
-            expect(ContentHome.data.content.type).toEqual("Single Video");
-        });
-    });
-
 })
 ;
