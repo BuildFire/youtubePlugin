d341d4bdd1000b84ed5e7c736be51698b1aa192d
sandeepchhapola <schhapola0001@gmail.com>
2015-08-19 18:11:29 +0530

    Showed item list background in widget section, Added 'getImageUrl' filter in widget/app.js

--- widget/controllers/widget.home.controller.js
+++ widget/controllers/widget.home.controller.js
@@ -2,15 +2,34 @@
 
 (function (angular) {
     angular.module('youtubePluginWidget')
-        .controller('WidgetHomeCtrl', ['DataStore', 'TAG_NAMES', 'STATUS_CODE', function (DataStore, TAG_NAMES, STATUS_CODE) {
-            var WidgetHome = this;
+        .controller('WidgetHomeCtrl', ['$filter', 'DataStore', 'TAG_NAMES', 'STATUS_CODE', function ($filter, DataStore, TAG_NAMES, STATUS_CODE) {
+            var WidgetHome = this
+                , currentItemListBgImage = null
+                , getImageUrlFilter = $filter("getImageUrl");
+
             WidgetHome.data = null;
+
+            var setCurrentItemListBgImage = function (_WidgetHomeData) {
+                var body = angular.element('body');
+                if (_WidgetHomeData.design && _WidgetHomeData.design.itemListBgImage && currentItemListBgImage != _WidgetHomeData.design.itemListBgImage) {
+                    currentItemListBgImage = _WidgetHomeData.design.itemListBgImage;
+                    body.css(
+                        'background', '#010101 url('
+                        + getImageUrlFilter(currentItemListBgImage, 342, 770, 'resizeImage')
+                        + ') repeat fixed top center')
+                } else if (_WidgetHomeData.design && !_WidgetHomeData.design.itemListBgImage) {
+                    currentItemListBgImage = null;
+                    body.css('background', 'none');
+                }
+            };
+
             /*
              * Fetch user's data from datastore
              */
             var init = function () {
                 var success = function (result) {
                         WidgetHome.data = result.data;
+                        setCurrentItemListBgImage(WidgetHome.data);
                     }
                     , error = function (err) {
                         if (err && err.code !== STATUS_CODE.NOT_FOUND) {
@@ -24,8 +43,9 @@
             var onUpdateCallback = function (event) {
                 if (event && event.tag === TAG_NAMES.YOUTUBE_INFO) {
                     WidgetHome.data = event.obj;
+                    setCurrentItemListBgImage(WidgetHome.data);
                 }
             };
-            DataStore.onUpdate().then(function(){},function(){},onUpdateCallback);
+            DataStore.onUpdate().then(null, null, onUpdateCallback);
         }])
 })(window.angular);
