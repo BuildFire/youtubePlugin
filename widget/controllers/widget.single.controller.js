21a053d20d445ead915a3ff8fb73749ce7f4bdd3
sandeepchhapola <schhapola0001@gmail.com>
2015-08-21 17:05:24 +0530

    On changing single video url, changed data in widget without refresh.

--- widget/controllers/widget.single.controller.js
+++ widget/controllers/widget.single.controller.js
@@ -1,65 +1,70 @@
 'use strict';
 
 (function (angular) {
-    angular.module('youtubePluginWidget')
-        .controller('WidgetSingleCtrl', ['$routeParams', '$filter', 'YoutubeApi', 'DataStore', 'TAG_NAMES', function ($routeParams, $filter, YoutubeApi, DataStore, TAG_NAMES) {
-            var currentItemDetailsBgImage = ''
-                , getImageUrlFilter = $filter("getImageUrl");
+  angular.module('youtubePluginWidget')
+    .controller('WidgetSingleCtrl', ['$routeParams', '$filter', 'YoutubeApi', 'DataStore', 'TAG_NAMES', 'Location', function ($routeParams, $filter, YoutubeApi, DataStore, TAG_NAMES, Location) {
+      var currentItemDetailsBgImage = ''
+        , getImageUrlFilter = $filter("getImageUrl");
 
-            var WidgetSingle = this;
-            WidgetSingle.data = null;
-            WidgetSingle.video = null;
+      var WidgetSingle = this;
+      WidgetSingle.data = null;
+      WidgetSingle.video = null;
 
-            var setCurrentItemListBgImage = function (_WidgetVideoData) {
-                var body = angular.element('body');
-                if (_WidgetVideoData.design && _WidgetVideoData.design.itemDetailsBgImage && currentItemDetailsBgImage != _WidgetVideoData.design.itemDetailsBgImage) {
-                    currentItemDetailsBgImage = _WidgetVideoData.design.itemDetailsBgImage;
-                    body.css(
-                        'background', '#010101 url('
-                        + getImageUrlFilter(currentItemDetailsBgImage, 342, 770, 'resize')
-                        + ') repeat fixed top center')
-                } else if (_WidgetVideoData.design && !_WidgetVideoData.design.itemDetailsBgImage) {
-                    currentItemDetailsBgImage = null;
-                    body.css('background', 'none');
-                }
-            };
+      var setCurrentItemListBgImage = function (_WidgetVideoData) {
+        var body = angular.element('body');
+        if (_WidgetVideoData.design && _WidgetVideoData.design.itemDetailsBgImage && currentItemDetailsBgImage != _WidgetVideoData.design.itemDetailsBgImage) {
+          currentItemDetailsBgImage = _WidgetVideoData.design.itemDetailsBgImage;
+          body.css(
+            'background', '#010101 url('
+            + getImageUrlFilter(currentItemDetailsBgImage, 342, 770, 'resize')
+            + ') repeat fixed top center')
+        } else if (_WidgetVideoData.design && !_WidgetVideoData.design.itemDetailsBgImage) {
+          currentItemDetailsBgImage = null;
+          body.css('background', 'none');
+        }
+      };
 
-            /*
-             * Fetch user's data from datastore
-             */
-            var init = function () {
-                var success = function (result) {
-                        WidgetSingle.data = result.data;
-                        setCurrentItemListBgImage(WidgetSingle.data);
-                    }
-                    , error = function (err) {
-                        console.error('Error while getting data', err);
-                    };
-                DataStore.get(TAG_NAMES.YOUTUBE_INFO).then(success, error);
-            };
-            init();
+      /*
+       * Fetch user's data from datastore
+       */
+      var init = function () {
+        var success = function (result) {
+            WidgetSingle.data = result.data;
+            setCurrentItemListBgImage(WidgetSingle.data);
+          }
+          , error = function (err) {
+            console.error('Error while getting data', err);
+          };
+        DataStore.get(TAG_NAMES.YOUTUBE_INFO).then(success, error);
+      };
+      init();
 
-            var getSingleVideoDetails = function (_videoId) {
-                var success = function (result) {
-                        WidgetSingle.video = result.items && result.items[0];
-                    }
-                    , error = function (err) {
-                        console.error('Error In Fetching Single Video Details', err);
-                    };
-                YoutubeApi.getSingleVideoDetails(_videoId).then(success, error);
-            };
-            if ($routeParams.videoId) {
-                getSingleVideoDetails($routeParams.videoId);
-            } else {
-                console.error('Undefined Video Id Provided');
-            }
+      var getSingleVideoDetails = function (_videoId) {
+        var success = function (result) {
+            WidgetSingle.video = result.items && result.items[0];
+          }
+          , error = function (err) {
+            console.error('Error In Fetching Single Video Details', err);
+          };
+        YoutubeApi.getSingleVideoDetails(_videoId).then(success, error);
+      };
+      if ($routeParams.videoId) {
+        getSingleVideoDetails($routeParams.videoId);
+      } else {
+        console.error('Undefined Video Id Provided');
+      }
 
-            var onUpdateCallback = function (event) {
-                if (event && event.tag === TAG_NAMES.YOUTUBE_INFO) {
-                    WidgetSingle.data = event.obj;
-                    setCurrentItemListBgImage(WidgetSingle.data);
-                }
-            };
-            DataStore.onUpdate().then(null, null, onUpdateCallback);
-        }])
+      var onUpdateCallback = function (event) {
+        if (event && event.tag === TAG_NAMES.YOUTUBE_INFO) {
+          WidgetSingle.data = event.obj;
+          if (WidgetSingle.data.content.videoID && (WidgetSingle.data.content.videoID !== $routeParams.videoId)) {
+            getSingleVideoDetails(WidgetSingle.data.content.videoID);
+          } else if (WidgetSingle.data.content.playListID) {
+            Location.goTo("#/feed/" + WidgetSingle.data.content.playListID);
+          }
+          setCurrentItemListBgImage(WidgetSingle.data);
+        }
+      };
+      DataStore.onUpdate().then(null, null, onUpdateCallback);
+    }])
 })(window.angular);
