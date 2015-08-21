6f1a164d2636f8a5e1da7eb786fc9815953ce414
sandeepchhapola <schhapola0001@gmail.com>
2015-08-21 12:35:30 +0530

    Showed dynamic data for a video like title, description etc

--- widget/controllers/widget.single.controller.js
+++ widget/controllers/widget.single.controller.js
@@ -2,14 +2,29 @@
 
 (function (angular) {
     angular.module('youtubePluginWidget')
-        .controller('WidgetSingleCtrl', ['$routeParams', 'YoutubeApi', function ($routeParams, YoutubeApi) {
+        .controller('WidgetSingleCtrl', ['$routeParams', 'YoutubeApi', 'DataStore', 'TAG_NAMES', function ($routeParams, YoutubeApi, DataStore, TAG_NAMES) {
             var WidgetSingle = this;
             WidgetSingle.data = null;
+            WidgetSingle.video = null;
+
+            /*
+             * Fetch user's data from datastore
+             */
+            var init = function () {
+                var success = function (result) {
+                        WidgetSingle.data = result.data;
+                        setCurrentItemListBgImage(WidgetSingle.data);
+                    }
+                    , error = function (err) {
+                        console.error('Error while getting data', err);
+                    };
+                DataStore.get(TAG_NAMES.YOUTUBE_INFO).then(success, error);
+            };
+            init();
 
             var getSingleVideoDetails = function (_videoId) {
                 var success = function (result) {
-                        WidgetSingle.data = result.items && result.items[0];
-                        console.info('-------------------Single Video Details data----------------', WidgetSingle.data);
+                        WidgetSingle.video = result.items && result.items[0];
                     }
                     , error = function (err) {
                         console.error('Error In Fetching Single Video Details', err);
