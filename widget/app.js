609ed085d9eac54145dc52d7d21951ee3c452a6d
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-10-21 15:30:58 +0530

    Implemented transitions in widget section

--- widget/app.js
+++ widget/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function (angular, buildfire) {
-  angular.module('youtubePluginWidget', ['ngRoute', 'infinite-scroll'])
+  angular.module('youtubePluginWidget', ['ngRoute', 'infinite-scroll','ngAnimate'])
     .config(['$routeProvider', '$compileProvider', function ($routeProvider, $compileProvider) {
 
       /**
