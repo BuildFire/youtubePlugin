b3ee6ab5601db04fcddd8667d3bcc6e38276493d
Mustafa Hamza <mustafahamza996@gmail.com>
2020-05-05 21:34:21 +0200

    BF Local storage checking for undefined fix.

--- widget/cache.js
+++ widget/cache.js
@@ -6,7 +6,7 @@ var cache = {
   getCache: function(callback) {
     window.buildfire.localStorage.getItem(`${this.storeName}_${window.bfInstanceId}`, (error, value) => {
       if (error) return callback(error, null);
-      callback(null, JSON.parse(value));
+      callback(null, JSON.parse((typeof value === 'undefined') ? null : value));
     });
   }
 };
